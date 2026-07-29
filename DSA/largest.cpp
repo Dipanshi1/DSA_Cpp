28172b7651448c7a62bfc751746841a697c36aec
Dipanshi1 <dipanshig6969@gmail.com>
2026-07-29 09:50:30 +0530

    largest.cpp

---
+++ DSA/largest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    int largest = a;
+    if (b > largest) {
+        largest = b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+    cout << largest << endl;
+    return 0;
+}
