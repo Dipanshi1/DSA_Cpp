95e33fb1c8c6ee0f8083119360dfae738349beca
Dipanshi1 <dipanshig6969@gmail.com>
2026-07-29 09:49:08 +0530

    vector1.cpp

---
+++ DS_lab_CSE17/vector1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>av[4];
+    av[0].push_back(1);
+    av[0].push_back(2);
+    av[0].push_back(3);
+    av[1].push_back(4);
+    av[1].push_back(5);
+    av[1].push_back(6);
+    av[1].push_back(7);
+    av[1].push_back(8);
+    av[2].push_back(9);
+    av[2].push_back(10);
+    av[3].push_back(11);
+    av[3].push_back(12);
+    av[3].push_back(13);
+    av[3].push_back(14);//insertion at the end
+    
+    for(int i=0;i<4; i++){
+        for (int j=0;j<av[i].size(); j++)
+            cout<<av[i][j]<<" ";
+
+        cout<<"\n";
+
+}
+
+}
