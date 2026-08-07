535c79212e21a7d0c809890bdde4150ba3f6f0e2
Dipanshi1 <dipanshig6969@gmail.com>
2026-08-08 01:43:49 +0530

    linear search with break

---
+++ DSA/lsbreak.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i<n; i++){
+        cin>> arr[i];
+    }
+     bool found = false;
+     int x;
+     cin>>x;
+     for (int i = 0; i<n; i++){
+         if (arr[i] == x){
+            found = true;
+        cout<<"found"<<endl;
+      break;
+     }
+    }
+    if(!found){
+        cout<<"Not Found"<<endl;    
+    }
+}
