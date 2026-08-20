df216547b3945261a44386dcb93f78b8a39823a7
Dipanshi1 <dipanshig6969@gmail.com>
2026-08-20 15:53:49 +0530

    outside loop freq count

---
+++ DSA/freq.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    bool visited[n];
+    for (int i=0; i<n; i++){
+    visited[i] = false;}
+    for(int i =0; i<n; i++){
+        cin>>arr[i];}
+        for(int i =0; i<n; i++){
+        if(visited[i])
+            continue;
+        int count = 0;
+        for(int j =0; j<n; j++){
+            if (arr[j]==arr[i]){
+                count++;
+                visited[j]=true;
+            }
+        }
+    if(count > 1){
+    cout << arr[i] << " " << count << endl;
+    }       
+  }
+}
